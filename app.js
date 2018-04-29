4132fc969fd0e9fc09d95b3f19f108304337c38d
xlfsummer <xlf-summer@qq.com>
2018-04-29 12:24:54 +0800

    获取 config

--- app.js
+++ app.js
@@ -1,34 +1,8 @@
-const CONFIG_FILE_NAME = "cli-dict.config.json";
 
-let fs = require('fs');
-let path = require('path');
-let readline = require('readline');
+let config = require("./scripts/configManager");
 
-/** @example 'C:\\Users\\username\\AppData\\Roaming' */
-let configPath = path.resolve(process.env.APPDATA, CONFIG_FILE_NAME);
-
-let configExist = fs.existsSync(configPath);
-
-if (!configExist) {
-    createConfig(configPath)
-}
-
-let config = readConfig(configPath);
 
 serach()
-
-
-
-function createConfig(configPath) { 
-    let rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
-
-    rl.question("123", answer => {
-        console.log(answer + 555);
-    })
-}
 
 function readConfig() { 
 
